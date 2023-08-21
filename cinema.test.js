34d04dc6614af3061db75e0b9a55a360bcf48f5c
Альберт <sabir116rus@gmail.com>
2023-08-21 13:16:25 +0300

    Completed 1 task

---
+++ cinema.test.js
@@ -0,0 +1,62 @@
+const {
+  selectDateTime,
+  orderTickets,
+  checkSeatIsTaken,
+} = require("./lib/util.js");
+const { getText } = require("./lib/commands");
+
+let page;
+let tomorrow = "nav.page-nav > a:nth-child(2)"; // Билеты на завтра
+let oneWeek = "nav.page-nav > a:nth-child(7)"; // Билеты через неделю
+let movieTime = "[data-seance-id='176']";
+let ticketHint = "body > main > section > div > p:nth-child(7)";
+let confirmingText =
+  "Покажите QR-код нашему контроллеру для подтверждения бронирования.";
+
+describe("Service for Movie tickets order", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/index.php");
+    await page.setDefaultNavigationTimeout(0);
+  });
+
+  afterEach(() => {
+    page.close();
+  });
+
+  test("Should order one ticket for Movie-1 tomorrow", async () => {
+    await selectDateTime(page, tomorrow, movieTime);
+    await orderTickets(page, 2, 5);
+    const actual = await getText(page, ticketHint);
+    expect(actual).toContain(confirmingText);
+  });
+
+  test("Should order three tickets for Movie-1 in a week", async () => {
+    await selectDateTime(page, oneWeek, movieTime);
+    await orderTickets(page, 2, 8, 9, 10);
+    const actual = await getText(page, ticketHint);
+    expect(actual).toContain(confirmingText);
+  });
+
+  test("Should try to order ticket for Movie-1 if seat is taken already", async () => {
+    await expect(async () => {
+      await selectDateTime(page, tomorrow, movieTime);
+      await orderTickets(page, 2, 5);
+    }).rejects.toThrowError("Seat(s) is taken");
+  });
+
+  test("Check if the place is taken after ordering ", async () => {
+    let row = 2;
+    let seat = 6;
+    await selectDateTime(page, oneWeek, movieTime);
+    await orderTickets(page, row, seat);
+    await page.goto("http://qamid.tmweb.ru/client/index.php");
+    await selectDateTime(page, oneWeek, movieTime);
+    await checkSeatIsTaken(page, row, seat);
+    const classExist = await page.$eval(
+      `div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${seat})`,
+      (el) => el.classList.contains("buying-scheme__chair_taken")
+    );
+    expect(classExist).toEqual(true);
+  });
+});
